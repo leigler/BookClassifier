222e99efb949e1644a0ca2a65f45b920905b710d
missingDomain <lukasld@mit.edu>
2019-08-27 10:24:02 -0400

    initial commit

---
+++ ObjectClassifier.js
@@ -0,0 +1,245 @@
+/*
+ * ObjectClassifier
+ */
+
+// RESULT in JSON format
+let classificationResults;
+
+/* 
+ * CONSTANTS
+ *
+ * @param url_cloud_api - cloud url of the model's locations.
+ * @param repeats - how many repeats of the same label until true?
+ * @param label_cutoffs - individual accuracy cutoffs (text lower than image) 
+ */
+
+var CONSTANTS = {
+    url_cloud_api : 'http://storage.googleapis.com/04e86dd04c0411c711039770034830af/model.json',
+    label_repeats : 5,
+    label_cutoffs : {   'Grace_Leslie': 0.80,
+                        'Evan_Ziporyn': 0.95,
+                        'Brendan_Landis': 0.95,
+                        'Azra_Aksamija': 0.95,
+                        'Tolini_Finamore': 0.95,
+                        'Dewa_Alit': 0.95,
+                        'Maya_Beiser': 0.95,
+                        'Tal_Danino': 0.95,
+                        'Lucy_McRae': 0.95,
+                        'Hyphen_Labs': 0.95,
+                        'Nadya_Peek': 0.95,
+                        'Fry_Reas': 0.95,
+                        'Pawel_Romanczuk': 0.95,
+                        'Victor_Gama': 0.95,
+                        'Arnold_Dreyblatt': 0.95
+                      }
+};
+
+/* 
+ * ObjectClassifier
+ *
+ * P5 - Instance Mode
+ * Handles the VideoStream, fetching of the Model as well as Classification 
+ */
+
+const ObjectClassifier = ( sketch ) => {
+
+    let classifier;
+    let video;
+    let CurrObject;
+
+    /* Preload
+     * creates Video Stream
+     * fetches Models from Cloud
+     * assigns video size
+     */
+    sketch.preload = () => {
+        video = sketch.createCapture(sketch.VIDEO);
+        classifier = ml5.imageClassifier(CONSTANTS.url_cloud_api);
+        video.size(window.innerWidth, window.innerHeight);
+
+        // Optional : tests if the browser is mobile
+        console.log("is the browser mobile : " + isMobile());
+        if(!isMobile()){
+            alert("This is a Mobile Application, please switch to a Mobile Browser");
+        }
+    };
+
+    
+    // first classification, creates LabelObject
+    sketch.setup = () => {
+        sketch.rotate(sketch.PI);
+        classifyVideo();
+        CurrObject = new LabelObject();
+    };
+
+    
+    // classifies Video 
+    function classifyVideo() {
+        classifier.classify(video, gotResult);
+    }
+
+    // called when results are in
+    function gotResult(err, results) {
+        if(err){
+            console.log(err);
+        }else{
+            classifyVideo();    
+            CurrObject.updateCurrObject(results[0]);
+            var results = CurrObject.cutoffUpdater(CONSTANTS.label_repeats);
+            if (results) classificationResults = resultJSON;
+
+
+            // Optional: Dev and Log display
+            if(results) {
+                createResultHTML();
+                console.log(classificationResults);
+            }
+
+
+        }
+    }
+};
+
+
+/* 
+ * Optional: creates Result Div
+ */
+
+function createResultHTML (){
+    var resulthtml = document.getElementById("result");
+    resulthtml.innerHTML = JSON.stringify(classificationResults);
+    resulthtml.style.display = 'none';
+}
+
+
+// ----- UTILS ----- //
+
+
+/* 
+ * isMobile
+ *
+ * checks if the browser is Mobile 
+ */
+
+function isMobile(){
+    if (/Mobi/.test(navigator.userAgent)) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+
+/* 
+ * Label Object
+ *
+ * Label Object to be updated at every Result
+ */
+
+class LabelObject {
+
+    constructor(){
+        this.label;
+        this.confidence;
+        this.curr_amount;
+        this.last_conf_amounts = [];
+    }
+
+
+    /* Parses and updates results from the classification 
+     * @param {Object} - classified Results 
+     */
+    updateCurrObject(object){
+        this.label = object.label;
+        this.confidence = object.confidence;
+    }
+}
+
+
+/* 
+ * Prototype cutoffUpdater
+ *
+ * counts the preivous appearances of labels depending on "repeats"
+ * returns the most common appearance after "repeats" times
+ *
+ * Info: For this class to run correctly, on each call updateCurrObject has to be called
+ * 
+ * @param {Integer} - repeats       the amount of repeats to be checked for
+ * @returns {Object}                the detected Object
+ */
+
+LabelObject.prototype.cutoffUpdater = function(repeats){
+
+    let cutoff_test = this.cutoff(this.confidence, CONSTANTS.label_cutoffs[this.label]);
+
+    if(cutoff_test){
+
+        this.last_conf_amounts.push(this.confidence);
+        this.curr_amount += 1;
+
+        if(this.curr_amount == repeats){
+
+            let mean_acc = this.last_conf_amounts.reduce((a, b) => a + b, 0)/repeats;
+
+            resultJSON.label = this.label;
+            resultJSON.repeats = repeats;
+            resultJSON.confidence = mean_acc;
+
+            this.reset();
+            return true;
+        }
+        
+    }else{
+        this.reset();
+    }
+};
+
+
+/* 
+ * Prototype rest
+ *
+ * resets the Label Object's parameters 
+ */
+
+LabelObject.prototype.reset = function(){
+
+    this.curr_amount = 0;    
+    this.last_conf_amounts = [];
+
+};
+
+
+/* 
+ * cutoff
+ * tests the current confidence with the threshold confidence
+ *
+ * @param curr_conf - current classified confidence 
+ * @param cutoff_conf - cutoff confidence as specified 
+ * @return returns true when curr confidence is larger than cutoff, else false
+ */
+
+LabelObject.prototype.cutoff = function(curr_conf, cutoff_conf){
+    if(curr_conf >= cutoff_conf){
+        return true;
+    }else{
+        return false;
+    }
+};
+
+
+/* 
+ * resultJSON
+ *
+ * @param label - label that appeared "repeats" times 
+ * @param repeats - how many repeats were we looking for?
+ * @param confidence - mean confidence of "repeats"
+ */
+
+var resultJSON = { 
+                   location: 'pages', 
+                   label : null,
+                   repeats : null,
+                   confidence : null
+                 };
+
